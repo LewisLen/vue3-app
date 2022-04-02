a18b947ca26ae1f59ac0f756294147123eca83c9
LewisLen <csshack2016@126.com>
2022-04-02 23:48:15 +0800

    feat: 新增axios element-plus 简单的示例

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-button type="primary" @click="query">查询</el-button>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="电影名" prop="title" width="180" />
+      <el-table-column label="评分" prop="rate" width="180" />
+      <el-table-column label="简介" prop="url" />
+      <el-table-column label="封面" prop="cover" />
+    </el-table>
   </div>
 </template>
+
+<script>
+import { reactive, toRefs, onMounted, getCurrentInstance } from "vue";
+
+export default {
+  setup() {
+    const currentInstance = getCurrentInstance();
+    const { $http } = currentInstance.appContext.config.globalProperties;
+
+    function query() {
+      $http({
+        url: "http://localhost:3030/movies/query",
+      }).then((res) => {
+        data.tableData = res.data.data;
+      });
+    }
+
+    const data = reactive({
+      tableData: [],
+    });
+    const refData = toRefs(data);
+
+    onMounted(() => {});
+    return {
+      ...refData,
+      query,
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="box">
+    <el-form>
+      <el-form-item label="姓名">
+        <el-input v-model="userName" placeholder="请设置姓名" maxlength="10" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          v-model="passWord"
+          placeholder="请设置密码"
+          type="password"
+          maxlength="12"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button type="primary" @click="register">注册</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { reactive, toRefs, onMounted, getCurrentInstance } from "vue";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    const currentInstance = getCurrentInstance();
+    const { $http } = currentInstance.appContext.config.globalProperties;
+
+    function register() {
+      $http({
+        url: "http://192.168.1.100:3030/users/register",
+        method: "POST",
+        data: {
+          userName: data.userName,
+          passWord: data.passWord,
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    }
+
+    function login() {
+      $http({
+        url: "http://192.168.1.100:3030/users/login",
+        method: "POST",
+        data: {
+          userName: data.userName,
+          passWord: data.passWord,
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    }
+
+    const data = reactive({
+      userName: "",
+      passWord: "",
+    });
+
+    const refData = toRefs(data);
+    onMounted(() => {});
+
+    return {
+      ...refData,
+      register,
+      login,
+    };
   },
 };
 </script>
+
+<style>
+.box {
+  width: 400px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-200px, -150px);
+}
+p {
+  margin-top: 5px;
+}
+.box-input {
+  height: 30px;
+  border: 1px solid #ccc;
+}
+</style>
